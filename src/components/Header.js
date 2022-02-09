6c7b3935898f85a9c73cbe25ea81ff8779e01f4c
Chloé R <clositi@live.fr>
2022-02-09 19:25:40 +0100

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import "../style/header.css"
+
+/**
+ * Header Component with Nav
+ * @returns {JSX} React component
+ */
+
+function Header() {
+	return (
+		<header className="header">
+			<Link className="header__link" to="/">
+				<img className="header__logo" src={Logo} alt="Logo de Wealth Health" />
+				<h1 className="header__title">HRnet</h1>
+			</Link>
+			<nav className="nav">
+				<NavLink exact className="nav__option" to="/employee-list">
+					View Current Employees
+				</NavLink>
+				<NavLink exact className="nav__option" to="/">
+					Home
+				</NavLink>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
