1bdbfd1c16285f644d48bfcea10f6d2f5762c686
Chloé R <clositi@live.fr>
2022-02-15 22:43:25 +0100

    added style to error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../style/error.css";
 
 /**
  * Error Page
@@ -9,11 +10,13 @@ import { Link } from "react-router-dom";
 function Error() {
 	return (
 		<main className="error">
-			<h1 className="error__title">404</h1>
-			<p className="error__info">Oups! La page que vous demandez n'existe pas.</p>
-			<Link className="error__link" to="/">
-				Retourner sur la page d'accueil
-			</Link>
+			<div className="error__container">
+				<h1 className="error__title">404</h1>
+				<p className="error__info">Oops! This page doesn't exist :( </p>
+				<Link className="error__link" to="/">
+					Go back to home page
+				</Link>
+			</div>
 		</main>
 	);
 }
