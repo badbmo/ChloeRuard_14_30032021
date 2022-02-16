2d221bb9e27f1e115c1aac2a2319951901582e13
Chloé R <clositi@live.fr>
2022-02-16 19:33:49 +0100

    added DataProvider in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './style/App.css';
+import "./style/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import { DataProvider } from "./utils/context/dataContext";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/employee-list" element={<Employees />} />
-				<Route path="/*" element={<Error />} />
-			</Routes>
-		</Router>
+		<DataProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/employee-list" element={<Employees />} />
+					<Route path="/*" element={<Error />} />
+				</Routes>
+			</Router>
+		</DataProvider>
 	);
 }
 
