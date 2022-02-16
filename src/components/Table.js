79247d5641d934af387902bcf8381ba878c3ff5e
Chloé R <clositi@live.fr>
2022-02-16 19:37:02 +0100

    added context to use originalData in Table corrected key with index for <td>

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../style/table.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Search from "./Search";
 import Entries from "./Entries";
 import Pagination from "./Pagination";
 import { tableHeadData } from "../utils/const/tableHeadData";
-import { bodyData } from "../utils/const/mockedData";
+// import { bodyData } from "../utils/const/mockedData";
 import { entriesValue } from "../utils/const/entriesValue";
 import useSortData from "../utils/hooks/useSortData";
+import { DataContext } from "../utils/context/dataContext";
 
 /**
  * Table Component to display current employees
@@ -18,6 +19,8 @@ function Table() {
 	const [itemsperpage, setItemsPerPage] = useState(10);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [query, setQuery] = useState("");
+
+	const {originalData} = useContext(DataContext);
 
 	//to avoid problem like: we are on current page 3/3 and number of page change to 1 (we stay on page 3/1)
 	const resetCurrentPage = () => {
@@ -32,21 +35,21 @@ function Table() {
 	};
 
 	//filter totalData (array of object), Object.keys(object) get all the keys of one object as an array, k as keys string to iterate on object: object["firstName"], object["lastName"] ...
-	const filteredData = bodyData.filter((object) => {
+	const filteredData = originalData.filter((object) => {
 		return Object.keys(object).some((k) => object[k].toLowerCase().includes(query.toLowerCase().trim()));
 	});
 
-	const { data, requestSort, sortedField } = useSortData(filteredData);
+	const { sortedData, requestSort, sortedField } = useSortData(filteredData);
 
 	const filteredDataLength = filteredData.length;
-	const originalDataLength = bodyData.length;
+	const originalDataLength = originalData.length;
 	const startItem = (currentPage - 1) * itemsperpage;
 	const endItem = currentPage * itemsperpage;
 
-	const displayedData = data.slice(startItem, endItem);
+	const displayedData = sortedData.slice(startItem, endItem);
 	const displayedDataLength = displayedData.length;
 	const firstDisplayedData = filteredDataLength === 0 ? 0 : startItem + 1;
-	const lastDisplayedData = data.indexOf(displayedData[displayedDataLength - 1]) + 1;
+	const lastDisplayedData = sortedData.indexOf(displayedData[displayedDataLength - 1]) + 1;
 
 	const tableHead = (list) => {
 		return list.map((item, index) => (
@@ -60,7 +63,7 @@ function Table() {
 		if (originalDataLength === 0) {
 			return (
 				<tr className="error__noData table__row">
-					<td className="row__cell" colspan="9">
+					<td className="row__cell" colSpan="9">
 						You are a company with no employee
 					</td>
 				</tr>
@@ -69,7 +72,7 @@ function Table() {
 		if (filteredDataLength === 0) {
 			return (
 				<tr className="error__noData table__row">
-					<td className="row__cell" colspan="9">
+					<td className="row__cell" colSpan="9">
 						No match found, sorry !
 					</td>
 				</tr>
@@ -79,8 +82,8 @@ function Table() {
 		//filter data (array of object), Object.values(object) get all the values of one object as an array, v as key to iterate on object: "value 1", "value 2" ...
 		return data.map((item, index) => (
 			<tr key={index} className="table__row">
-				{Object.values(item).map((v) => (
-					<td key={v} className="row__cell">
+				{Object.values(item).map((v, index) => (
+					<td key={index} className="row__cell">
 						{v}
 					</td>
 				))}
