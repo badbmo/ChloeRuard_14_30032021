604bb44c5e229a01ed96b8a2e1ab2425a07a2096
Chloé R <clositi@live.fr>
2022-02-16 01:17:06 +0100

    created useSortData hook

---
+++ src/utils/hooks/useSortData.js
@@ -0,0 +1,49 @@
+import { useState, useMemo } from "react";
+
+/**
+ * Custom hook to sort Data from table
+ * @param {string} data data to sort
+ * @param {string} config to choose a sorting by default, set by default to null
+ * @returns {object} error, isloading, data
+ */
+
+ export default function useSortData (data, config = null) {
+	const [sortedField, setSortedField] = useState(config);
+  
+  const sortedItems = useMemo(() => {
+    let sortableItems = [...data];
+		if (sortedField !== null) {
+			sortableItems.sort((a, b) => {
+				if (sortedField.name === "zipCode") {
+					return sortedField.direction === "ascending"
+						? a[sortedField.name] - b[sortedField.name]
+						: b[sortedField.name] - a[sortedField.name];
+				}
+				if (sortedField.name === "startDate" || "birthDate") {
+					return sortedField.direction === "ascending"
+						? new Date(a[sortedField.name]) - new Date(b[sortedField.name])
+						: new Date(b[sortedField.name]) - new Date(a[sortedField.name]);
+				}
+				if (a[sortedField.name] < b[sortedField.name]) {
+					return sortedField.direction === "ascending" ? -1 : 1;
+				}
+				if (a[sortedField.name] > b[sortedField.name]) {
+					return sortedField.direction === "ascending" ? -1 : 1;
+				}
+				return 0;
+			});
+		}
+    return sortableItems;
+  }, [data, sortedField]);
+
+	const requestSort = (headCellClicked) => {
+		let direction = "ascending";
+		if (sortedField && sortedField.name === headCellClicked && sortedField.direction === "ascending") {
+			direction = "descending";
+		}
+		setSortedField({ name: headCellClicked, direction });
+	};
+
+
+  return { data: sortedItems, requestSort, sortedField};
+}
