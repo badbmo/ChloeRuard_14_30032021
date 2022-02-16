6996705ca5918963b395fce26cd5571cc8dd1f58
Chloé R <clositi@live.fr>
2022-02-16 01:14:50 +0100

    corrected id of date of birth in tableHeadData

--- src/utils/const/tableHeadData.js
+++ src/utils/const/tableHeadData.js
@@ -3,7 +3,7 @@ export const tableHeadData = [
 	{ name: "Last Name", id: "lastName" },
 	{ name: "Start Date", id: "startDate" },
 	{ name: "Department", id: "department" },
-	{ name: "Date of Birth", id: "dateOfBirth" },
+	{ name: "Date of Birth", id: "birthDate" },
 	{ name: "Street", id: "street" },
 	{ name: "City", id: "city" },
 	{ name: "State", id: "state" },
