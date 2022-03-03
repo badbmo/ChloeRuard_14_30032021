ac587fa8042d0c72ff917c6feb86a4833d63c586
Chloé R <clositi@live.fr>
2022-03-03 23:34:56 +0100

    published on gitpages (hashrouter)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./style/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
